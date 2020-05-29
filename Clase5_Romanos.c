29dbd29300d33b78a5c8a4396a240a03ebdeb5fc
AlejandroValZ <64992989+AlejandroValZ@users.noreply.github.com>
2020-05-29 17:49:47 -0400

    Update Clase5_Romanos.c

--- Clase5_Romanos.c
+++ Clase5_Romanos.c
@@ -3,84 +3,54 @@
 void main(){
 	int num, unidades, decenas, centenas, miles;
 	int i = 0;
-	int j =0;
+	int j = 0;
 	printf("Escriba un numero entre 1 y 3999\n");
 	while(i < 1){
 		scanf("%d", &num);
-		if(num<1||num>4000){
+		if(num < 1 || num > 4000){
 			printf("Escriba un numero valido\n");
 		}else{
-			i =1;
+			i = 1;
 		}
 	}
 	
-	unidades=num%10;
-	num=num/10;
-	decenas=num%10;
-	num=num/10;
-	centenas=num%10;
-	miles=num/10;
+	unidades = num % 10;
+	num = num / 10;
+	decenas = num % 10;
+	num = num / 10;
+	centenas = num % 10;
+	miles = num / 10;
 	
-	int c = centenas-5;
-	int d = decenas -5;
-	int u = unidades-5;
+	int c = centenas - 5;
+	int d = decenas - 5;
+	int u = unidades - 5;
 	
-	for(j = 0; j < miles; j++){
+	for(j = 0 ; j < miles ; j++){
 		printf("M");
 	}
 	
-	i=0;
-	j=0;
+	convertirARomano(centenas, c, "C", "CD", "D", "CM");
+	convertirARomano(decenas, d, "X", "XL", "L", "XC");
+	convertirARomano(unidades, u, "I", "IV", "V", "IV");
 	
-	if(centenas==9){
-		printf("CM");
-	} else if(centenas>=5){
-		printf("D");
-		for(i = 0; i < c; i++){
-			printf("C");
+}
+
+void convertirARomano(int posicionDigitos, int menosCinco, char uno[1], char cuatro[2], char cinco[1], char nueve[2]){
+	int i = 0;
+	int j = 0;
+	
+	if(posicionDigitos == 9){
+		printf("%s", nueve);
+	} else if(posicionDigitos >= 5){
+		printf("%s", cinco);
+		for(i = 0; i < menosCinco ; i++){
+			printf("%s", uno);
 		}
-	}else if(centenas==4){
-		printf("CD");
+	}else if(posicionDigitos == 4){
+		printf("%s", cuatro);
 	}else{
-		for(j = 0; j < centenas; j++){
-			printf("C");
+		for(j = 0 ; j < posicionDigitos ; j++){
+			printf("%s", uno);
 		}
 	}
-	
-	i=0;
-	j=0;
-	
-	if(decenas==9){
-		printf("XC");
-	} else if(decenas>=5){
-		printf("L");
-		for(i = 0; i < d; i++){
-			printf("X");
-		}
-	}else if(decenas==4){
-		printf("XL");
-	}else{
-		for(j = 0; j < decenas; j++){
-			printf("X");
-		}
-	}
-	
-	i=0;
-	j=0;
-	
-	if(unidades==9){
-		printf("IX");
-	} else if(unidades>=5){
-		printf("V");
-		for(i = 0; i < u; i++){
-			printf("I");
-		}
-	}else if(unidades==4){
-		printf("IV");
-	}else{
-		for(j = 0; j < unidades; j++){
-			printf("I");
-		}
-	}
-	
 }
